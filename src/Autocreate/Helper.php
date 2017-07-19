48d4c0fd159df6e65b5313a690f2ee58496191e9
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-07-20 01:54:52 +0300

    Добавлена возможность отката созданного highload-блока.

---
+++ src/Autocreate/Helper.php
@@ -0,0 +1,28 @@
+<?php
+namespace BitrixMigrations\Autocreate
+{
+    use Bitrix\Highloadblock\HighloadBlockTable;
+    use Bitrix\Main\Application;
+
+    /**
+     * Содержит вспомогательных методов для шаблонов миграций.
+     */
+    class Helper
+    {
+        /**
+         * Получает ID highload-блока по имени его таблицы.
+         * @param  string  $table Имя таблицы.
+         * @return integer        ID.
+         */
+        public static function getHighloadIdByTable($table)
+        {
+            $highloads = HighloadBlockTable::getTableName();
+            $db = Application::getConnection();
+
+            $result = $db->query("SELECT * FROM $highloads WHERE TABLE_NAME = '$table';");
+            $result = $result->fetch();
+
+            return $result['ID'];
+        }
+    }
+}
